819e59c3378337da4336788537fa01d5397c3d74
J. Derksen <jip.derksen@hva.nl>
2022-08-30 16:27:19 +0200

    added the loops

--- reserve_i_guess.ino
+++ reserve_i_guess.ino
@@ -1,11 +1,10 @@
 #include <FastLED.h>
 
-// How many leds in your strip?
 #define NUM_LEDS 60
 
 #define DATA_PIN 11
 #define CLOCK_PIN 12
-#define BRIGHTNESS          1000
+#define BRIGHTNESS 1000
 
 const int buttonPin = 2;  
 
@@ -13,403 +12,39 @@ const int buttonPin = 2;
 int buttonState = 0;       
 long Kleur = CRGB::Red;
 
-# ik ben geweldig
-
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
+    FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  {
-    if (buttonState == HIGH) {
-      leds[0] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[1] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[2] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[3] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[4] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[5] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[6] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[7] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[8] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[9] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[10] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[11] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[12] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[13] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[14] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[15] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[16] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[17] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[18] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[0] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[19] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[20] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[21] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[22] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[23] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[24] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[25] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[26] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[27] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[28] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[29] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[30] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[31] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[32] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[33] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[34] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[35] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[36] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[37] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[38] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[39] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[40] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[41] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[42] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[43] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[44] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[45] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[46] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[47] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[48] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[49] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[50] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[51] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[52] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[53] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[54] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[55] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[56] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[57] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[58] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[59] = Kleur;
-      FastLED.show();
-      delay(5);
-      leds[60] = Kleur;
-      FastLED.show();
-      delay(5);
-    }
-  }
-
-  delay(1000);
-
-  {
-    // read the state of the pushbutton value:
     buttonState = digitalRead(buttonPin);
-
-    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-    if (buttonState == HIGH) {
-      // Now turn the LED off, then pause
-      leds[60] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[59] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[58] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[57] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[56] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[55] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[54] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[53] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[52] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[51] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[50] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[49] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[48] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[47] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[46] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[45] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[44] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[43] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[42] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[41] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[40] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[39] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[38] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[37] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[36] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[35] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[34] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[33] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[32] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[31] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[30] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[29] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[28] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[27] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[26] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[25] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[24] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[23] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[22] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[21] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[20] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[19] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[18] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[17] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[16] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[15] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[14] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[13] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[12] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[11] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[10] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[9] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[8] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[7] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[6] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[5] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[4] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[3] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[2] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[1] = CRGB::Black;
-      FastLED.show();
-      delay(5);
-      leds[0] = CRGB::Black;
-      FastLED.show();
-      delay(5);
+    {
+        if (buttonState == HIGH) {
+            for (int i = 0; i <= NUM_LEDS; i++) {
+                leds[i] = Kleur;
+                FastLED.show();
+                delay(5);
+            }
+        }
     }
-  }
 
-  delay(1000);
+    delay(1000);
 
+    {
+        buttonState = digitalRead(buttonPin);
+
+        if (buttonState == HIGH) {
+            // Now turn the LED off, then pause
+            for (int i = NUM_LEDS; i >= 0; i--) {
+                leds[i] = CRGB::Black;
+                FastLED.show();
+                delay(5);
+            }
+        }
+    }
+
+    delay(1000);
 }
